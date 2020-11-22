4005c600379bc88589cbf38d798969846fd44d5c
Deepak Singh <32263182+deepaksing@users.noreply.github.com>
2020-11-22 14:42:09 +0000

    Update urls.py

--- note_task/note_task/urls.py
+++ note_task/note_task/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
+    path('addtask/', views.addtask, name='addtask')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
